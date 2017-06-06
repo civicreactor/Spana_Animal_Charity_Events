4b0f908dfd386c5fe1afa30b88621be515b3ea7d
antonio <ammj24@hotmail.com>
2017-06-06 20:19:04 +0100

    added button for zoom message

---
+++ spana-app/web/IWAD-app/src/app/services/message.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Observable';
+
+import * as firebase from 'firebase';
+
+@Injectable()
+export class MessageService {
+  postKey: any;
+  constructor(private http: Http) { }
+
+  post(name, coordinates, message) {
+    this.postKey = firebase.database().ref('/users').push({ name, coordinates, message }).key;
+    return this.postKey;
+  }
+}
